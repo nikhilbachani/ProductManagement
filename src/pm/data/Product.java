fc562a112f5dc7a6e9f8177b71e590366136ccab
Nikhil Bachani <nikhilbachani25@gmail.com>
2021-04-12 08:02:42 +0000

    Add custom constructors to the Product class

--- src/pm/data/Product.java
+++ src/pm/data/Product.java
@@ -18,6 +18,7 @@ package pm.data;
 
 import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
+import static pm.data.Rating.*;
 
 /**
  * {@code Product} class represents properties and behaviors of a product in
@@ -45,6 +46,21 @@ public class Product {
     private String name;
     private BigDecimal price;
     private Rating rating;
+
+    public Product() {
+        this(0, "No name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
 
     public int getId() {
         return id;
