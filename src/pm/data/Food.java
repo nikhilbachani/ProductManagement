b39a3eb57b984695ce977341c18e5395a5182d74
Nikhil Bachani <nikhilbachani25@gmail.com>
2021-04-16 08:04:09 +0000

    Override methods and use polymorphism: getDiscount (Product class)

--- src/pm/data/Food.java
+++ src/pm/data/Food.java
@@ -42,6 +42,12 @@ public class Food extends Product {
     }
 
     @Override
+    public BigDecimal getDiscount() {
+        return bestBefore.isEqual(LocalDate.now())
+                ? super.getDiscount() : BigDecimal.ZERO;
+    }
+
+    @Override
     public String toString() {
         return super.toString() + ", " + bestBefore;
     }
